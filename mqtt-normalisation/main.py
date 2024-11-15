a01d1172df1f889c3fe2d0f99f29960956a9f4fa
Tomas Neubauer <tomas@quix.ai>
2024-11-15 10:44:13 +0100

    MQTT normalisation

--- mqtt-normalisation/main.py
+++ mqtt-normalisation/main.py
@@ -5,17 +5,35 @@ from quixstreams import Application
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Application(consumer_group="transformation-v1", auto_offset_reset="earliest")
+app = Application(
+    consumer_group="mqtt-norm-v1", 
+    auto_offset_reset="earliest",
+    processing_guarantee="exactly_once")
 
-input_topic = app.topic(os.environ["input"])
+input_topic = app.topic(os.environ["input"], value_deserializer="bytes", key_deserializer="str")
 output_topic = app.topic(os.environ["output"])
+
 
 sdf = app.dataframe(input_topic)
 
-# put transformation logic here
-# see docs for what you can do
-# https://quix.io/docs/get-started/quixtour/process-threshold.html
+sdf = sdf.filter(lambda row, key, *_: len(key.split("-")) == 6, metadata=True)
 
+def expand_key(row, key, timestamp, headers):
+    
+    expanded_key = key.split("-")
+    
+    return {
+        "device_id": expanded_key[3],
+        "sensor": expanded_key[0],
+        "axis": expanded_key[2],
+        "location": expanded_key[4],
+        "timestamp": timestamp,
+        "value": bytes.decode(row)
+    }
+
+sdf = sdf.apply(expand_key, metadata=True)
+sdf = sdf.set_timestamp(lambda row, *_: row["timestamp"])
+sdf = sdf.drop("timestamp")
 sdf.print()
 sdf.to_topic(output_topic)
 
