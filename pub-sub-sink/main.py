0caeb6595c9ae6b2ce4082c8e29075f61b8621dd
Tomas Neubauer <tomas@quix.io>
2024-11-15 08:21:55 +0000

    Edited 'main.py'

--- pub-sub-sink/main.py
+++ pub-sub-sink/main.py
@@ -1,24 +1,34 @@
 import os
+from google.api_core import retry
+from google.cloud.pubsub_v1.types import PublisherOptions
 from quixstreams import Application
-from quixstreams.sinks.community.bigquery import BigQuerySink
+from quixstreams.sinks.community.bigquery import PubSubSink
+import json
 
 # for local dev, load env vars from a .env file
 from dotenv import load_dotenv
 load_dotenv()
 
-TABLE_NAME = os.environ["TABLE_NAME"]
+TOPIC_ID = os.environ["TOPIC_ID"]
 PROJECT_ID = os.environ["PROJECT_ID"]
-DATASET_ID = os.environ["DATASET_ID"]
-DATASET_LOCATION = os.environ["DATASET_LOCATION"]
 SERVICE_ACCOUNT_JSON = os.environ["SERVICE_ACCOUNT_JSON"]
 
 
-bigquery_sink = BigQuerySink(
+# Configure the sink
+pubsub_sink = PubSubSink(
     project_id=PROJECT_ID,
-    location=DATASET_LOCATION,
-    dataset_id=DATASET_ID,
-    table_name=TABLE_NAME,
+    topic_id=TOPIC_ID,
+    # Optional: service account credentials as a JSON string
     service_account_json=SERVICE_ACCOUNT_JSON,
+    # Optional: customize serialization and flush timeout
+    value_serializer=json.dumps,
+    key_serializer=str,
+    flush_timeout=10,
+    # Optional: Additional keyword arguments are passed to the PublisherClient
+    publisher_options=PublisherOptions(
+        # Configure publisher options to retry on any exception
+        retry=retry.Retry(predicate=retry.if_exception_type(Exception)),
+    )
 )
 
 app = Application(
@@ -30,7 +40,7 @@ app = Application(
 input_topic = app.topic(os.environ["input"])
 
 sdf = app.dataframe(input_topic)
-sdf.sink(bigquery_sink)
+sdf.sink(pubsub_sink)
 
 if __name__ == "__main__":
     app.run(sdf)
