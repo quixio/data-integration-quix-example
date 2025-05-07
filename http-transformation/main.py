1c8ff6f2c312bd4f70bd72c907cde667d978f7e8
Tomas Neubauer <tomas@quix.io>
2025-05-07 13:57:30 +0000

    Edited 'main.py'

--- http-transformation/main.py
+++ http-transformation/main.py
@@ -9,7 +9,7 @@ app = Application(consumer_group="v1.1",
             auto_offset_reset="earliest", 
             use_changelog_topics=False)
 
-input_topic = app.topic(os.environ["input"])
+input_topic = app.topic(os.environ["input"], key_deserializer="str")
 output_topic = app.topic(os.environ["output"])
 
 sdf = app.dataframe(input_topic)
@@ -21,6 +21,18 @@ sdf = sdf.tumbling_window(3600, 3000).collect().final()
 
 sdf = sdf.apply(lambda row: sorted(row["value"], key=lambda row: row["time"]), expand=True)
 
+def transpose(row, key, *_):
+
+    for axis, value in row["values"].items():
+        yield {
+            "device_id": key,
+            "sensor": row["name"],
+            "axis": axis,
+            "location": "na",
+            "timestamp": row["time"]
+        }
+
+sdf = sdf.apply(transpose, metadata=True, expand=True) 
         
 # Print JSON messages in console.
 sdf.print()
